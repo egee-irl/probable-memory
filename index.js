3b15c74dc4b81982a8fe9805300e75fcde76a4f1
egee-irl <brian@egee.io>
2017-03-06 15:46:41 -0800

    no more hardcoded paths

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 
 document.getElementById("myButton").addEventListener("click", () => {
     const execFile = require('child_process').execFile;
-    const child = execFile('dosbox',['/home/egee/Dos/Raptor/RAP.EXE', '-exit'], (error, stdout, stderr) => {
+    const child = execFile('dosbox',[process.env.HOME+'/Dos/Raptor/RAP.EXE', '-exit'], (error, stdout) => {
     if (error) {
         throw error;
     }
