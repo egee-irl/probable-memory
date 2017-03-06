5ddcda8681c75018a9827b98534dffac13dc5afd
egee-irl <brian@egee.io>
2017-03-06 13:35:06 -0800

    added hook for DosBox

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
-document.getElementById("thing").addEventListener("click", () => {
-    console.log("logged.")
+// const spawn = require('child_process').spawn;
+// const ls = spawn('ls', ['-lh', '/usr']);
+
+document.getElementById("myButton").addEventListener("click", () => {
+    const execFile = require('child_process').execFile;
+    const child = execFile('dosbox', (error, stdout, stderr) => {
+    if (error) {
+        throw error;
+    }
+    console.log(stdout);
+    });
 });
