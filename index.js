3dfe495c84c33569289055143d938c3b4c80a2ee
egee-irl <brian@egee.io>
2017-03-07 07:57:04 -0800

    added oregon trail & missing file error handling

--- index.js
+++ index.js
@@ -1,9 +1,35 @@
+const fs = require('fs')
+const {dialog} = require('electron').remote
+const execFile = require('child_process').execFile
+
 document.getElementById('raptor').addEventListener('click', () => {
-  const execFile = require('child_process').execFile
-  execFile('dosbox', [process.env.HOME + '/Dos/Raptor/RAP.EXE', '-exit'], (error, stdout) => {
-    if (error) {
-      throw error
+  fs.access(process.env.HOME + '/Dos/Raptor/RAP.EXE', (err) => {
+    if (err) {
+      console.log(dialog.showErrorBox('Executable Not Found', 'Raptor executable was not found at: ' + process.env.HOME + '/Dos/Raptor/RAP.EXE'))
+    } else {
+      console.log('Raptor executable found at: ' + process.env.HOME + '/Dos/Raptor/RAP.EXE')
+      execFile('dosbox', [process.env.HOME + '/Dos/Raptor/RAP.EXE', '-exit'], (error, stdout) => {
+        if (error) {
+          throw error
+        }
+        console.log(stdout)
+      })
     }
-    console.log(stdout)
+  })
+})
+
+document.getElementById('oregontrail').addEventListener('click', () => {
+  fs.access(process.env.HOME + '/Dos/the-oregon-trail/OREGON.EXE', (err) => {
+    if (err) {
+      console.log(dialog.showErrorBox('Executable Not Found', 'Oregon Trail executable was not found at: ' + process.env.HOME + '/Dos/the-oregon-trail/OREGON.EXE'))
+    } else {
+      console.log('Raptor executable found at: ' + process.env.HOME + '/Dos/the-oregon-trail/OREGON.EXE')
+      execFile('dosbox', [process.env.HOME + '/Dos/the-oregon-trail/OREGON.EXE', '-exit'], (error, stdout) => {
+        if (error) {
+          throw error
+        }
+        console.log(stdout)
+      })
+    }
   })
 })
