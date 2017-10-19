79a96772122c874244b5c07ae80393563c686198
egee-irl <brian@egee.io>
2017-10-18 19:07:56 -0700

    first game

--- main.rb
+++ main.rb
@@ -1,10 +1,11 @@
 require 'shoes'
 
-Shoes.app width: 640, height: 320 do
+Shoes.app width: 320, height: 320 do
 
   stack(margin: 8) do
-    button 'Button 1' do background "#F3F".."#F90" end
-    button 'Button 2' do puts 'click!' end
-    button 'Button 3' do alert 'Nice click!' end
+    # button 'Button 1' do background "#F3F".."#F90" end
+    # button 'Button 2' do puts 'click!' end
+    # button 'Button 3' do alert 'Nice click!' end
+      button 'Commander Keen 4' do `dosbox "/home/egee/.dosgames/Commander Keen - Keen Dreams/keend/START.EXE" -exit -fullscreen` end
   end
 end
