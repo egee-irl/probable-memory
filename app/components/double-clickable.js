d39279c00a63bb6a0671a2fae689ed84e591ffa4
egee-irl <brian@egee.io>
2018-11-05 23:15:05 -0800

    fixed issue with using node modules

--- app/components/double-clickable.js
+++ app/components/double-clickable.js
@@ -1,6 +1,5 @@
 import Component from "@ember/component";
-import { os } from "os";
-// const os = require("os");
+const os = requireNode("os");
 
 export default Component.extend({
   doubleClick() {
