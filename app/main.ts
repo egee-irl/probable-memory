a2bf66157dfd260bbddfdf393cd620134e25395c
egee-irl <brian@egee.io>
2019-09-26 01:05:58 -0700

    added

---
+++ app/main.ts
@@ -0,0 +1,26 @@
+import { app, BrowserWindow } from "electron";
+import * as path from "path";
+
+let mainWindow: Electron.BrowserWindow;
+
+function createWindow() {
+  // Create the browser window.
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+  });
+
+  // and load the index.html of the app.
+  mainWindow.loadFile(path.join(__dirname, "./index.html"));
+
+  // Open the DevTools.
+  mainWindow.webContents.openDevTools();
+
+  // Emitted when the window is closed.
+  mainWindow.on("closed", () => {
+    // Dereference the window object, usually you would store windows
+    // in an array if your app supports multi windows, this is the time
+    // when you should delete the corresponding element.
+    mainWindow = null!;
+  });
+}
