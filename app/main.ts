7fdac762e0d8ea016fe70247d20f135568d2be3f
egee-irl <brian@egee.io>
2019-09-26 01:22:12 -0700

    concise example

--- app/main.ts
+++ app/main.ts
@@ -4,23 +4,12 @@ import * as path from "path";
 let mainWindow: Electron.BrowserWindow;
 
 function createWindow() {
-  // Create the browser window.
-  mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
-  });
+  mainWindow = new BrowserWindow({ width: 800, height: 600, });
 
-  // and load the index.html of the app.
   mainWindow.loadFile(path.join(__dirname, "./index.html"));
-
-  // Open the DevTools.
   mainWindow.webContents.openDevTools();
 
-  // Emitted when the window is closed.
-  mainWindow.on("closed", () => {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    mainWindow = null!;
-  });
+  mainWindow.on("closed", () => mainWindow = null!);
 }
+
+app.on("ready", createWindow);
